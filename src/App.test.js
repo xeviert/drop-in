2058c68e8ce3813eee1d1d81283b29c0a96a1c0d
Lopez, Richard (Contractor) <richard.lopez@yum.com>
2021-08-23 16:54:46 -0500

    test commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders App', () => {
+    expect(true).toEqual(false);
+    const div = document.createElement('div');
+    render(
+      <Router>
+        <App />
+      </Router>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
