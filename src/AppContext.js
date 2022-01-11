3797629da55b41699fe2f4355201cf0a0137b047
xevier <xevierturrubiartes@gmail.com>
2022-01-11 09:19:36 -0600

    added services to help with register adndnd ler

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import mockStudios from './store/studios.json';
 import mockTeachers from './store/teachers.json';
+import TokenService from './services/token-service';
+import IdleService from './services/idle-service';
 
-export const AppContext = React.createContext();
+export const AppContext = React.createContext({
+  user: {},
+  error: null,
+  setError: () => {},
+  clearError: () => {},
+  setUser: () => {},
+  processLogin: () => {},
+  processLogout: () => {},
+});
 
 export default class AppProvider extends Component {
   constructor(props) {
     super(props);
-    this.state = {
+    const state = {
       teachers: mockTeachers,
       studios: mockStudios,
+      user: {},
+      error: null,
     };
-  }
 
+    const jwtPayload = TokenService.parseAuthToken();
+
+    if (jwtPayload)
+      state.user = {
+        id: jwtPayload.user_id,
+        name: jwtPayload.name,
+        username: jwtPayload.sub,
+      };
+
+    this.state = state;
+    IdleService.setIdleCallback(this.logoutBecauseIdle);
+  }
 
   render() {
     return (
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+    API_ENDPOINT: '',
+    TOKEN_KEY: 'my-little-secret',
+  }
+  
