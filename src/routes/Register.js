f04eb0fcd992bfd1c6badd10f20a3380720902bb
xeviert <xevierturrubiartes@gmail.com>
2023-03-02 15:27:08 +0100

    shaping up teachers component

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -4,7 +4,7 @@ export default function Register() {
 
     return (
         <div>
-            
+            Register
         </div>
     )
 }
--- src/routes/Teachers.js
+++ src/routes/Teachers.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import { AppContext } from '../AppContext';
 import Collapsible from '../components/Collapsible';
 
+import '../styling/Teachers.css';
+
 /** @jsxImportSource theme-ui */
 
 export default function Teachers() {
@@ -10,33 +12,39 @@ export default function Teachers() {
 
   const allTeachers = teachers.map((t) => {
     return (
-      <div className='p-3 col-md'>
-        <div key={t.id}>{t.full_name}</div>
-        <div>
+      <div className='teacher-item'>
+      <div className='p-3 col-md' >
+        <div key={t.id} className='full-name'>{t.full_name}</div>
+        <div className='location'>
           {t.city},{' '}{t.state}
         </div>
-        <a href={t.website}>{t.website}</a>
+        <div>
+        <img src={t.photo_source}  />
+        </div>
         <Collapsible>
+        <a href={t.website}>{t.website}</a>
         <div>Certifications: {t.certifications}</div>
         <div>Yoga Types: {t.yoga_types}</div>
         </Collapsible>
       </div>
-
+      </div>
     );
   });
 
   return (
     <div className='page-body'>
       <h1>Teachers</h1>
+      <div className='page-content'>
       <div
         sx={{
           display: 'grid',
           gridGap: 4,
           gridTemplateColumns: ['repeat(2, 1fr)', 'repeat(4, 1fr)'],
         }}
-      >
+        >
         {allTeachers}
       </div>
+        </div>
     </div>
   );
 }
